bd091035335a6ae82068bea16921637d5f6d1ecf
Soyzian <158862852+Soyzian@users.noreply.github.com>
2026-05-24 13:21:40 -0300

    Actualizar backend.cpp

--- backend.cpp
+++ backend.cpp
@@ -1,3 +1,13 @@
+/*
+ * Viewport - Sistema de entorno gráfico minimalista
+ * Copyright (C) 2026 VNT
+ *
+ * Este programa es software libre: puedes redistribuirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General de GNU según es publicada
+ * por la Free Software Foundation, ya sea la versión 3 de la Licencia,
+ * o (a tu elección) cualquier versión posterior.
+ */
+
 #include "backend.h"
 
 Backend::Backend(QObject *parent) : QObject(parent), m_currentProcess(nullptr) {}
